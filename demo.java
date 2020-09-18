0e0133bd31c0b04cd13493d92c1d7e735464b98f
TsheringDuba03 <12190094.gcit@rub.edu.bt>
2020-09-18 21:56:21 +0600

    first commit

---
+++ demo.java
@@ -0,0 +1,12 @@
+
+public class demo{
+	public static void main(String args[]){
+ 
+	int[] a = {1,2,3,4,5,6,7,8,9,10};
+	for(int i=0; i<a.length; i++)
+		{
+			System.out.println(a[i]);
+		}
+			System.out.println("length:" + a.length);
+	} 
+}
